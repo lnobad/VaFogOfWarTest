e2c05247ca33c1342ddf0d2aae9da1ca76e2c5c4
Aleksey Lyashenko <lightmap.lyashenko@gmail.com>
2020-04-29 23:35:15 +0300

    init repo

---
+++ Source/VaFogOfWarTest/VaFogOfWarTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VaFogOfWarTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAFOGOFWARTEST_API AVaFogOfWarTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
